611724c41d57f1804a3412fc798f7f5e80521464
Mahann <mahann@kth.se>
2021-04-16 14:16:33 +0200

    test

--- main.c
+++ main.c
@@ -6,13 +6,9 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
-#include "gridSystem.h"
 
-#define WINDOW_WIDTH (1600)
+#define WINDOW_WIDTH (1200)
 #define WINDOW_HEIGHT (800)
-
-void renderBackground(SDL_Renderer *gRenderer, SDL_Texture *mTile, SDL_Rect gTiles[]);
-void loadMedia(SDL_Renderer *gRenderer,SDL_Texture **mTiles, SDL_Rect gTiles[]);
 
 
 int WinMain(void){
@@ -20,7 +16,6 @@ int WinMain(void){
     // Setup
     //-------------------------------------------
     // Setup
-    SDL_Renderer *gRenderer = NULL;
 
     if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0)
     {
@@ -38,25 +33,14 @@ int WinMain(void){
 	    return 1;
     }
 
-    // create a renderer, which sets up the graphics hardware
-    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
-    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
-    if (!rend){
-        printf("error creating renderer: %s\n", SDL_GetError());
-        SDL_DestroyWindow(win);
-        SDL_Quit();
-        return 1;
-    }
-    SDL_Rect gTiles[16];
-    SDL_Texture *mTiles = NULL;
-    // set to 1 when window close button is pressed
-    int close_requested = 0;
 
-    loadMedia(gRenderer, &mTiles, gTiles);
 
     // End of Setup
     //-------------------------------------------
     // Start of continuing render-loop 
+
+    // set to 1 when window close button is pressed
+    int close_requested = 0;
 
     while (!close_requested)
     {
@@ -70,46 +54,10 @@ int WinMain(void){
                 close_requested = 1;
                 break;
             }
-            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-            SDL_RenderClear(gRenderer);
-            renderBackground(gRenderer, mTiles, gTiles);
+
         }
     }
 
-    SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
     SDL_Quit();
 }
-
-
-//
-//Function which renders the whole game-field
-//
-void renderBackground(SDL_Renderer *gRenderer, SDL_Texture *mTiles, SDL_Rect gTiles[]){
-    
-    SDL_Rect possition;
-    possition.y = 0;
-    possition.x = 0;
-    possition.h = getTileHeight();
-    possition.w = getTileWidth();
-    
-    for (int i = 0; i<getTileColumns(); i++) {
-        for (int j = 0; j<getTileRows(); j++) {
-            possition.y = i*getTileHeight();
-            possition.x = j*getTileWidth();
-            SDL_RenderCopyEx(gRenderer, mTiles, &gTiles[getTileGrid(i,j)],&possition , 0, NULL, SDL_FLIP_NONE);
-        }
-    }
-}
-
-void loadMedia(SDL_Renderer *gRenderer, SDL_Texture **mTiles, SDL_Rect gTiles[]){
-    SDL_Surface* gTilesSurface = IMG_Load("resources/groundTextures/grass1.PNG");
-    *mTiles = SDL_CreateTextureFromSurface(gRenderer, gTilesSurface);
-    for (int i = 0; i < 16; i++) {
-        gTiles[i].x = i*getTileWidth();
-        gTiles[i].y = 0;
-        gTiles[i].w = getTileWidth();
-        gTiles[i].h = getTileHeight();
-    }
-}
-
