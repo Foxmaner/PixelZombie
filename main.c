73a20103e2aa5b5235efecb7159db1eefa64b622
Mahann <mahann@kth.se>
2021-04-19 17:29:48 +0200

    Code formatting

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 //v0.1
-//Create map
+//Project start: 2021-04-15
+//Created by Eskil B, Marcus M, Carl G, Anton C and Markus H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,32 +8,25 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
-
 #include "map.h"
 
 #define WINDOW_WIDTH (1024)
 #define WINDOW_HEIGHT (1024)
 
-
 void renderBackground(SDL_Renderer *renderer, SDL_Texture *mTile, SDL_Rect gTiles[]);
 void loadMedia(SDL_Renderer *renderer, SDL_Texture **mTiles, SDL_Rect gTiles[]);
 
 int WinMain(void){
-    
     // Setup
     //-------------------------------------------
     // Setup
 
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0){
         printf("error initializing SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* win = SDL_CreateWindow("Pixel Zombie",
-                                       SDL_WINDOWPOS_CENTERED,
-                                       SDL_WINDOWPOS_CENTERED,
-                                       WINDOW_WIDTH, WINDOW_HEIGHT,0);
+    SDL_Window* win = SDL_CreateWindow("Pixel Zombie", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT,0);
     if (!win){
         printf("error creating window: %s\n", SDL_GetError());
         SDL_Quit();
@@ -47,41 +41,35 @@ int WinMain(void){
     // Initilize background
     SDL_Texture *mTiles = NULL;
     SDL_Rect gTiles[16];
-   
-   
-
-
 
     // End of Setup
     //-------------------------------------------
-    // Start of continuing render-loop 
-
+    // Start of continuing render-loop
+    loadMedia(renderer, &mTiles, gTiles);
+    
     // set to 1 when window close button is pressed
     int close_requested = 0;
-
-    loadMedia(renderer, &mTiles, gTiles);
-
-    while (!close_requested)
-    {
+    //Game event
+    while (!close_requested){
         // process events
         SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                close_requested = 1;
+        while (SDL_PollEvent(&event)){
+            switch (event.type){
+                case SDL_QUIT:
+                    close_requested = 1;
                 break;
             }
-
         }
+
+        //Game logic
+
+
+        //Game rendering
         SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(renderer);
         renderBackground(renderer, mTiles, gTiles);
         SDL_RenderPresent(renderer);
     }
-
-    
 
    // SDL_DestroyWindow(win);
    // SDL_Quit();
@@ -94,8 +82,8 @@ void renderBackground(SDL_Renderer *renderer, SDL_Texture *mTiles, SDL_Rect gTil
     position.h = getTileHeight();
     position.w = getTileWidth();
     
-    for (int i = 0; i<getTileColumns(); i++) {
-        for (int j = 0; j<getTileRows(); j++) {
+    for (int i = 0; i<getTileColumns(); i++){
+        for (int j = 0; j<getTileRows(); j++){
             position.y = i*getTileHeight();
             position.x = j*getTileWidth();
             SDL_RenderCopyEx(renderer, mTiles, &gTiles[getTileGrid(i,j)],&position , 0, NULL, SDL_FLIP_NONE);
@@ -104,19 +92,12 @@ void renderBackground(SDL_Renderer *renderer, SDL_Texture *mTiles, SDL_Rect gTil
 }
 
 void loadMedia(SDL_Renderer *renderer, SDL_Texture **mTiles, SDL_Rect gTiles[]){
-
     SDL_Surface* gTilesSurface = IMG_Load("resources/TILES.PNG");
     *mTiles = SDL_CreateTextureFromSurface(renderer, gTilesSurface);
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < 16; i++){
         gTiles[i].x = i*getTileWidth();
         gTiles[i].y = 0;
         gTiles[i].w = getTileWidth();
         gTiles[i].h = getTileHeight();
-    } 
-    
+    }
 }
-
-
-
-
-
