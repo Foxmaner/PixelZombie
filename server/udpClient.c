1a1db940d4698c975095a51d6b5fbfe77c405cbe
Marcus Medoc <mmedoc@kth.se>
2021-04-28 09:27:50 +0200

    Created datapacket struct

--- server/udpClient.c
+++ server/udpClient.c
@@ -4,35 +4,39 @@
 #include "udpClient.h"
 #include "SDL2/SDL_net.h"
  
-void sendData(int x_cord, int y_cord)
-{
+void sendData(int x_cord, int y_cord){
 
     UDPsocket sd;
 	IPaddress srvadd;
 	UDPpacket *p;
     UDPpacket *p2;
 
-     if (SDLNet_Init() < 0)
-	{
+	struct data_packet{
+		int nrOfP;
+		int pXPoss[4];
+		int pYPoss[4];
+		int nrOfZ;
+		int zXPoss[6];
+		int zYPoss[6];
+	};typedef struct data_packet dataPacket;
+
+	if (SDLNet_Init() < 0){
 		fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
 		//exit(EXIT_FAILURE);
 	}
 
-    if (!(sd = SDLNet_UDP_Open(0)))
-	{
+    if (!(sd = SDLNet_UDP_Open(0))){
 		fprintf(stderr, "SDLNet_UDP_Open: %s\n", SDLNet_GetError());
 		//exit(EXIT_FAILURE);
 	}
 
     /* Resolve server name  */
-	if (SDLNet_ResolveHost(&srvadd, "185.76.9.42", 2000) == -1)
-	{
-		fprintf(stderr, "SDLNet_ResolveHost(185.76.9.42 2000): %s\n", SDLNet_GetError());
+	if (SDLNet_ResolveHost(&srvadd, "81.230.227.193", 2000) == -1){
+		fprintf(stderr, "SDLNet_ResolveHost(81.230.227.193 2000) : % s\n ", SDLNet_GetError());
 		//exit(EXIT_FAILURE);
 	}
 
-    if (!((p = SDLNet_AllocPacket(512))&& (p2 = SDLNet_AllocPacket(512))))
-	{
+    if (!((p = SDLNet_AllocPacket(512))&& (p2 = SDLNet_AllocPacket(512)))){
 		fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
 		//exit(EXIT_FAILURE);
 	}
@@ -46,7 +50,4 @@ void sendData(int x_cord, int y_cord)
 		    p->address.port = srvadd.port;	/* And destination port */
 		    p->len = strlen((char *)p->data) + 1;
             SDLNet_UDP_Send(sd, -1, p);
-
-             
-        
 } 
