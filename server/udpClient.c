a986c08db200fb702fcad23407c71ad3aa1e6a4d
Eskil Brännerud <eskilb@kth.se>
2021-04-29 13:31:46 +0200

    Bugfix: Only initilizes connection 1 time

--- server/udpClient.c
+++ server/udpClient.c
@@ -8,18 +8,8 @@
 	IPaddress srvadd;
 	UDPpacket *p;
     UDPpacket *p2; 
-void sendData(int x_cord, int y_cord, char selectedIp[100]){
 
-
-
-	struct data_packet{
-		int nrOfP;
-		int pXPoss[4];
-		int pYPoss[4];
-		int nrOfZ;
-		int zXPoss[6];
-		int zYPoss[6];
-	};typedef struct data_packet dataPacket;
+void createConnection(char selectedIp[100]){
 
 	if (SDLNet_Init() < 0){
 		fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
@@ -36,19 +26,33 @@ void sendData(int x_cord, int y_cord, char selectedIp[100]){
 		fprintf(stderr, "SDLNet_ResolveHost(81.230.227.193 2000) : % s\n ", SDLNet_GetError());
 		//exit(EXIT_FAILURE);
 	}
+	
 
-    if (!((p = SDLNet_AllocPacket(512))&& (p2 = SDLNet_AllocPacket(512)))){
-		fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
-		//exit(EXIT_FAILURE);
-	}
+}
+
+void sendData(int x_cord, int y_cord, char selectedIp[100]){
+
+
+
+	if(sd!=NULL){
+		if (!((p = SDLNet_AllocPacket(512))&& (p2 = SDLNet_AllocPacket(512)))){
+			fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
+			//exit(EXIT_FAILURE);
+		}
 
 
             // send and retrive positions  
         
             
-            sprintf((char *)p->data, "%d %d\n", (int) x_cord, (int) y_cord);    
-            p->address.host = srvadd.host;	/* Set the destination host */
-		    p->address.port = srvadd.port;	/* And destination port */
-		    p->len = strlen((char *)p->data) + 1;
-            SDLNet_UDP_Send(sd, -1, p);
-} 
+		sprintf((char *)p->data, "%d %d\n", (int) x_cord, (int) y_cord);    
+		p->address.host = srvadd.host;	/* Set the destination host */
+		p->address.port = srvadd.port;	/* And destination port */
+		p->len = strlen((char *)p->data) + 1;
+		SDLNet_UDP_Send(sd, -1, p);
+}
+
+	else{
+		createConnection(selectedIp);
+		sendData(x_cord, y_cord, selectedIp);
+	}
+}
