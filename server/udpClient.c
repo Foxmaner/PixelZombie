e9344068829f85a47fb229b54424f3d2d9dccdaa
Mahann <mahann@kth.se>
2021-05-10 17:52:45 +0200

    formatting

--- server/udpClient.c
+++ server/udpClient.c
@@ -10,49 +10,36 @@ UDPpacket *p;
 UDPpacket *p2;
 int playerID;
 
-
-void createConnection(char selectedIp[100])
-{
-
-	if (SDLNet_Init() < 0)
-	{
+void createConnection(char selectedIp[100]){
+	if (SDLNet_Init() < 0){
 		fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
 		//exit(EXIT_FAILURE);
 	}
 
-	if (!(sd = SDLNet_UDP_Open(0)))
-	{
+	if (!(sd = SDLNet_UDP_Open(0))){
 		fprintf(stderr, "SDLNet_UDP_Open: %s\n", SDLNet_GetError());
 		//exit(EXIT_FAILURE);
 	}
 
 	/* Resolve server name  */
-	if (SDLNet_ResolveHost(&srvadd, "127.0.0.1", 2000) == -1)
-	{
+	if (SDLNet_ResolveHost(&srvadd, "127.0.0.1", 2000) == -1){
 		fprintf(stderr, "SDLNet_ResolveHost(81.230.227.193 Detta är eskils ip ba fuck han 2000) : % s\n ", SDLNet_GetError());
 		//exit(EXIT_FAILURE);
 	}
 }
 
-void sendData(int flag, int x_cord, int y_cord, char selectedIp[100], int playerID)
-{
-
+void sendData(int flag, int x_cord, int y_cord, char selectedIp[100], int playerID){
 	if (sd != NULL){
-		if (!((p = SDLNet_AllocPacket(512)) && (p2 = SDLNet_AllocPacket(512))))
-		{
+		if (!((p = SDLNet_AllocPacket(512)) && (p2 = SDLNet_AllocPacket(512)))){
 			fprintf(stderr, "SDLNet_AllocPacket: %s\n", SDLNet_GetError());
 			//exit(EXIT_FAILURE);
 		}
-
 		// send and retrive positions
-
 		sprintf((char *)p->data, "%d %d %d %d\n", (int)flag, (int)playerID, (int)x_cord, (int)y_cord);
 		p->address.host = srvadd.host; /* Set the destination host */
 		p->address.port = srvadd.port; /* And destination port */
 		p->len = strlen((char *)p->data) + 1;
 		SDLNet_UDP_Send(sd, -1, p);
-
-		
 	}
 	else{
 		createConnection(selectedIp);
@@ -76,9 +63,7 @@ void reciveData(char selectedIp[100], int kordinater[3]){
 				kordinater[2] = b;
 			}else if(flag == 1){
 				printf("Skott avfyrades av spelare: %d ", recivedID);
-			}
-			
-			
+			}		
 		}
 		else{
 			kordinater[0] = -1000;
@@ -90,7 +75,6 @@ void reciveData(char selectedIp[100], int kordinater[3]){
 		createConnection(selectedIp);
 		reciveData(selectedIp, kordinater);
 	}
-	
 }
 
 int reciveID(char selectedIp[100]){
@@ -111,5 +95,4 @@ int reciveID(char selectedIp[100]){
 		reciveID(selectedIp);
 	}
 	return -1;
-	
 }
